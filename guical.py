6944e8a3d00d07af2e967fd5e154123bf3699274
hanminxing <37164314+hanminxing@users.noreply.github.com>
2018-03-12 14:29:37 +0800

    GUI v1.0 beta
    GUI calculator初步版本

--- guical.py
+++ guical.py
@@ -8,13 +8,45 @@ import tkinter as tk
 from tkinter import ttk, scrolledtext,Menu
 from tkinter import messagebox as mBox
 
+import datetime
+import calendar
+import numpy as np
+import xlrd
+import pandas as pd
+import re
+
+#calculator core
+pd.set_option('colheader_justify', 'right')
+pd.set_option('display.unicode.east_asian_width', True)
+
+#date list generator
+def gen_date_list(start_date, end_date):
+    date_list = []
+    if end_date.month >= start_date.month:
+        y = 0
+    else:
+        y = 1
+        
+    months = (end_date.year - y - start_date.year)*12 + end_date.month - start_date.month + y*12
+    #print(months)
+    
+    for i in range(months+1):
+        month = start_date.month - 1 + i
+        year = start_date.year + month // 12
+        month = month % 12 + 1
+        day = min(start_date.day,calendar.monthrange(year,month)[1])
+        date_list.append(datetime.date(year,month,day))
+    return date_list
+
+#date_pattern = re.compile()
+
+#calculator GUI
 win = tk.Tk()
 
 win.title("IRR calculator")
 win.resizable(0,0)
 mainFrame = ttk.LabelFrame(win, text="@author: Hanminxing")
 mainFrame.grid(column=0, row=0,columnspan=4,rowspan=7, padx=8, pady=6)
-
  
 projectReqments = ["Total Money","Guarentee Fee","Service Fee","StartDate"]
 itemMoney = ['a','b','c','d']
@@ -38,11 +70,13 @@ irr_res.grid(column=3,row=4,padx=4,pady=3,sticky=tk.W)
 Reqment = {}
 dates_rents_l = []
 
+#submit button
 def submit():
     global dates_rents_l, Reqment
     Reqment = {}
     dates_rents_l = []
     
+    irr_res.delete(0, tk.END)
     for req in range(4):
         Reqment[projectReqments[req]] = itemMoney[req].get()
         
@@ -51,10 +85,52 @@ def submit():
         if len(date_rent) != 0:
             dates_rents_l.append(date_rent.split("\t"))
     
-    results.configure(text=dates_rents_l)
-    irr_res.insert(1,"123123")
-    irr_res.configure(state='readonly')
-    print(Reqment, dates_rents_l)
+    start_date = datetime.datetime.strptime(Reqment['StartDate'], "%Y/%m/%d")
+    end_date = datetime.datetime.strptime(dates_rents_l[-1][0],"%Y/%m/%d")
+    
+    col = ["项目金额","保证金", "管理费", "日期", "租金"]
+    date_list = gen_date_list(start_date, end_date)#数列
+    n = len(date_list)
+    blank_data = np.zeros((n,5))
+    df = pd.DataFrame(blank_data, columns=col)
+
+    df.项目金额[0] = float(Reqment['Total Money'].replace(",",""))
+    df.保证金[0] = float(Reqment['Guarentee Fee'].replace(",",""))
+    df.管理费[0] = float(Reqment['Service Fee'].replace(",",""))
+    #第一期现金流
+    df.租金[0] = -df.项目金额[0] + df.保证金[0] + df.管理费[0]
+    
+    df.日期 = date_list
+    
+    #填写对应日期的租金
+    for i in range(len(dates_rents_l)):
+            date = datetime.datetime.strptime(dates_rents_l[i][0],'%Y/%m/%d')
+            #print(date.date(), dates_rents_l[i][1])
+            for j in range(len(df.index)):
+                #print(df.日期[j], date.date())
+                if df.日期[j] == date.date():
+                    df.租金[j] = float(dates_rents_l[i][1].replace(",",""))
+                    #print(dates_rents_l[i][1])
+                    if j == len(df.index)-1:
+                        df.租金[j] = float(dates_rents_l[i][1].replace(",","")) - float(df.保证金[0])
+                        
+    payment = df.租金.values.tolist()   
+    print(payment)         
+    irr = round(np.irr(payment)*12,4)*100
+    print("IRR = %.2f" % irr)
+    
+    irr_res.insert(0,irr)
+    #irr_res.configure(state='readonly')
+    print(start_date, end_date,dates_rents_l)
+
+
+
+
+
+
+
+
+
 
 
 submit = ttk.Button(mainFrame, text="submit", command=submit).grid(column=0,row=4,padx=4,pady=3)
